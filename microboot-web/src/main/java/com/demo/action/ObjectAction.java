e50ac09778b1fa06c6bbb96885a7c078e5e9c043
64466 <644669785@qq.com>
2023-03-17 16:29:34 +0800

    update

---
+++ microboot-web/src/main/java/com/demo/action/ObjectAction.java
@@ -0,0 +1,46 @@
+package com.demo.action;
+
+import org.eclipse.jetty.server.ServletAttributes;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.context.request.RequestContextHolder;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.util.HashMap;
+import java.util.Map;
+
+@RestController
+@RequestMapping("/object/*")
+public class ObjectAction {
+    @RequestMapping("first")
+    public Object firstShow(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse) {
+        Map<String, Object> result = new HashMap<>();
+
+        result.put("【request】contextPath", httpServletRequest.getContextPath());
+        result.put("【request】messageParam", httpServletRequest.getParameter("message"));
+        result.put("【request】method", httpServletRequest.getMethod());
+        result.put("【session】sessionId", httpServletRequest.getSession().getId());
+        result.put("【application】virtualServerName", httpServletRequest.getServletContext().getVirtualServerName());
+        result.put("【application】initParameter", httpServletRequest.getServletContext().getInitParameter("teacher"));
+        return result;
+    }
+
+    @RequestMapping("second")
+    public Object secondShow(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse) {
+//         = (ServletAttributes)RequestContextHolder.getRequestAttributes();
+
+        Map<String, Object> result = new HashMap<>();
+
+        result.put("【request】contextPath", httpServletRequest.getContextPath());
+        result.put("【request】messageParam", httpServletRequest.getParameter("message"));
+        result.put("【request】method", httpServletRequest.getMethod());
+        result.put("【session】sessionId", httpServletRequest.getSession().getId());
+        result.put("【application】virtualServerName", httpServletRequest.getServletContext().getVirtualServerName());
+        result.put("【application】initParameter", httpServletRequest.getServletContext().getInitParameter("teacher"));
+        return result;
+    }
+
+}

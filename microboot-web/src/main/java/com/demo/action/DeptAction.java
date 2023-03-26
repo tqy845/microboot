c87a88651cf9538e16dcc0ee44b42d52c3870565
= <644669785@qq.com>
2023-03-26 12:08:26 +0800

    update

--- microboot-web/src/main/java/com/demo/action/DeptAction.java
+++ microboot-web/src/main/java/com/demo/action/DeptAction.java
@@ -2,10 +2,10 @@ package com.demo.action;
 
 import com.demo.vo.Company;
 import com.demo.vo.Dept;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import javax.annotation.Resource;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -15,10 +15,10 @@ public class DeptAction {
 
 
     @RequestMapping("add")
-    public Object add(Company company , Dept dept){
-        Map<String , Object> map = new HashMap<>();
-        map.put("dept" , dept);
-        map.put("company" , company);
+    public Object add(@ModelAttribute("company") Company company, @ModelAttribute("dept") Dept dept) {
+        Map<String, Object> map = new HashMap<>();
+        map.put("dept", dept);
+        map.put("company", company);
         return map;
     }
 }
--- microboot-web/src/main/java/com/demo/advice/GlobalDataPrefixAdvice.java
+++ microboot-web/src/main/java/com/demo/advice/GlobalDataPrefixAdvice.java
@@ -1,9 +1,20 @@
 package com.demo.advice;
 
+import org.springframework.web.bind.WebDataBinder;
 import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.InitBinder;
 
 @ControllerAdvice
 public class GlobalDataPrefixAdvice { // 全局数据预处理
+    @InitBinder("company")
+    public void company(WebDataBinder webDataBinder) {
+        webDataBinder.setFieldDefaultPrefix("company.");
+    }
+
+    @InitBinder("dept")
+    public void dept(WebDataBinder webDataBinder) {
+        webDataBinder.setFieldDefaultPrefix("dept.");
+    }
 
 
 }

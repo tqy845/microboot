9cb88dbad625215b21e4bbdabf3423678bafdacd
谭期元 <tqy@aigs.ltd>
2023-07-20 17:13:55 +0800

    取消junit-vintage-engine的引入，该组件是JUnit4中使用的测试引擎，它允许在JUnit 5平台上运行旧版本的JUnit测试。

--- microboot-web/src/test/java/com/demo/TestMessageEvent.java
+++ microboot-web/src/test/java/com/demo/TestMessageEvent.java
@@ -1,26 +1,30 @@
 package com.demo;
 
+import com.demo.event.YootkEvent;
+import com.demo.vo.Message;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.ApplicationEventPublisher;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.context.web.WebAppConfiguration;
 
 @ExtendWith(SpringExtension.class)  // 使用Junit5的测试工具
 @WebAppConfiguration // 启动Web运行环境
 @SpringBootTest(classes = StartSpringBootApplication.class)
-public class MessageEventTest {
-//    @Autowired
-//    private ApplicationEventPublisher publisher;
+public class TestMessageEvent {
+    @Autowired
+    private ApplicationEventPublisher publisher;
 
     @Test
     public void testEvent() {
         System.out.println("测试进行...");
-//        this.publisher.publishEvent(
-//                new YootkEvent(
-//                        this,
-//                        new Message("www.aigs.ltd","demo")
-//                )
-//        );
+        this.publisher.publishEvent(
+                new YootkEvent(
+                        this,
+                        new Message("www.aigs.ltd","demo")
+                )
+        );
     }
 }
